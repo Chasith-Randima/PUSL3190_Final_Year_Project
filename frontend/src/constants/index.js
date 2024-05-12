4237b0257b219a477ae74049be759a3485c95384
Randima-Silva <randimadevelopment@gmail.com>
2024-05-12 13:53:05 +0530

    for footer

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,30 @@
+export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "randimasilva.vercel.app" },
+        { title: "Featured", url: "randimasilva.vercel.app" },
+        { title: "Partnership", url: "randimasilva.vercel.app" },
+        { title: "Bussiness Relation", url: "randimasilva.vercel.app" },
+      ],
+    },
+    {
+      title: "University",
+      links: [
+        { title: "NSBM", url: "randimasilva.vercel.app" },
+        { title: "Blog", url: "randimasilva.vercel.app" },
+        { title: "Podcast", url: "randimasilva.vercel.app" },
+        { title: "Invite a friend", url: "randimasilva.vercel.app" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Discord", url: "randimasilva.vercel.app" },
+        { title: "Instagram", url: "randimasilva.vercel.app" },
+        { title: "Twitter", url: "randimasilva.vercel.app" },
+        { title: "Facebook", url: "randimasilva.vercel.app" },
+      ],
+    },
+  ];
+  
